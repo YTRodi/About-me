b0d2756226ce9cb60d1d3494df1210c6ea86f622
YTRodi <yagorodi@gmail.com>
2022-08-21 10:50:33 -0300

    Initial Commit

---
+++ About Me app/About_Me_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_Me_appApp.swift
+//  About Me app
+//
+//  Created by Yago Rodi on 21/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct About_Me_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
